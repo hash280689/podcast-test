ba9450a8c9d20749b83d66376d51e6a60ae52f45
Hashim Yaqub <hashimkyaqub@gmail.com>
2025-11-24 11:51:07 +0000

    Added in all major element tags

--- feed.py
+++ feed.py
@@ -14,8 +14,28 @@ rss_element = xml_tree.Element('rss', {'version':'2.0', 'xmlns:itunes':'http://w
 
 channel_element = xml_tree.SubElement(rss_element, 'channel')
 
+#Create the link prefix (My github page)
+link_prefix = yaml_data['link']
+
 #Create a "Title" element inside the channel element sub tree and then retrieve the actual title from the feed
 xml_tree.SubElement(channel_element, 'title').text = yaml_data['title']
+
+#Do this with all of the other elements
+#Note that some of the elements need the "itunes:" prefix
+xml_tree.SubElement(channel_element, 'format').text = yaml_data['format']
+xml_tree.SubElement(channel_element, 'subtitle').text = yaml_data['subtitle']
+xml_tree.SubElement(channel_element, 'itunes:author').text = yaml_data['author']
+xml_tree.SubElement(channel_element, 'description').text = yaml_data['description']
+
+#image is referenced with href so it needs the prefix. Concat them together
+xml_tree.SubElement(channel_element, 'itunes:image', {'href': link_prefix + yaml_data['image']})
+
+xml_tree.SubElement(channel_element, 'language').text = yaml_data['language']
+xml_tree.SubElement(channel_element, 'link').text = link_prefix
+
+#Creating a sub category tag which is stored in attributes
+xml_tree.SubElement(channel_element, 'itunes:category', {'text': yaml_data['category']})
+
 
 #Create a variable to store the output
 output_tree = xml_tree.ElementTree(rss_element)
